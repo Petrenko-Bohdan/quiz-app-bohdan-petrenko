bf9e954c6e20dbb2e3b1ef7226bc99e48aaecc01
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-02-19 21:26:43 +0100

    Set up application routing with StartScreen and QuizScreen components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { QuizScreenComponent } from './quiz-screen/quiz-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{path: '', component: StartScreenComponent},
+	{path: 'start-screen', component: StartScreenComponent},
+	{path: 'quiz', component: QuizScreenComponent}
+];
