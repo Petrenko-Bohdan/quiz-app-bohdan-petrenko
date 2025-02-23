8b19cd08e3704bec4f0bfe9eedb337ac183e4269
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-02-23 15:39:47 +0100

    add summary and answers screens to routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { QuizScreenComponent } from './quiz-screen/quiz-screen.component';
+import { SummaryScreenComponent } from './summary-screen/summary-screen.component';
+import { AnswersScreenComponent } from './answers-screen/answers-screen.component';
+
 
 export const routes: Routes = [
 	{path: '', component: StartScreenComponent},
 	{path: 'start-screen', component: StartScreenComponent},
-	{path: 'quiz', component: QuizScreenComponent}
+	{path: 'quiz', component: QuizScreenComponent},
+	{path: 'summary', component: SummaryScreenComponent}, 
+	
+	{path: 'answers', component: AnswersScreenComponent}
 ];
