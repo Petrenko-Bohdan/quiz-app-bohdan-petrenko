1f61585e81926d00778e5ad730f181d3ed74ad13
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-02-23 15:36:17 +0100

    extend appConfig with InactivityService and provideRouter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { InactivityService } from './app/core/services/inactivity.service';
 
-bootstrapApplication(AppComponent, appConfig)
+const updatedConfig = {
+  ...appConfig,
+  providers: [...appConfig.providers, provideRouter([]), InactivityService] // Добавляем сервисы в appConfig
+};
+
+bootstrapApplication(AppComponent, updatedConfig)
   .catch((err) => console.error(err));
+
