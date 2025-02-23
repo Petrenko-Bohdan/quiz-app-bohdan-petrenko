35e828e35ffba51949646534cbcc079bf6f629c1
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-02-23 17:11:06 +0100

    add TranslateModule

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Router} from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule} from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, TranslateModule],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss'
 })
@@ -17,5 +19,4 @@ export class StartScreenComponent {
 	startQuiz(){
 		this.router.navigate(['/quiz']);
 	}
-
 }
